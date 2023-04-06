726c41c5b39a420440d7dc26fd4da75b77684348
Colin Kennedy <colinvfx@gmail.com>
2023-04-05 20:36:04 -0700

    Removed unneeded vim-gitgutter.lua customization file

--- after/plugin/vim-gitgutter.lua
+++
@@ -1,8 +0,0 @@
--- Force gitgutter to update any time I save so make sure that signs are current
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern = "*",
-        command = "GitGutter",
-    }
-)
