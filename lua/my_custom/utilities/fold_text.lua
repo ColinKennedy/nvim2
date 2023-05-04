27d1f0f423ed815a0a675f18d0eea5b206587dbb
Colin Kennedy <colinvfx@gmail.com>
2023-05-03 22:59:22 -0700

    Added treesitter-based fold text

---
+++ lua/my_custom/utilities/fold_text.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+local _get_indent = function(node)
+    local start_row, start_column, _, _ = node:range()
+
+    local current = node:parent()
+
+    while current ~= nil
+    do
+        local start_current_row, start_current_column, _, _ = current:range()
+
+        if start_current_row ~= start_row
+        then
+            break
+        end
+
+        current = current:parent()
+        start_column = math.min(start_column, start_current_column)
+    end
+
+    return string.rep("·", start_column)
+end
+
+function M.get_summary(start_line, end_line)
+    buffer = 0
+    column = 0
+
+    local node = vim.treesitter.get_node({bufnr=buffer, pos={start_line, column}})
+    local indent = _get_indent(node)
+    local text = vim.treesitter.get_node_text(node, buffer)
+    local left_stripped = string.gsub(text, "^%s+", "")
+    local summary_line = left_stripped
+
+    for line in left_stripped:gmatch("([^\n]*)\n?")
+    do
+        summary_line = line
+
+        break
+    end
+
+    local stripped = string.gsub(summary_line, "%s+$", "")
+    local line_count_text = "[" .. (end_line - start_line) .. " lines]"
+    local formatted = string.format("%-68s %s", stripped, line_count_text)
+
+    return indent .. "<" .. formatted .. ">"
+end
+
+return M
