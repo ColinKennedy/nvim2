3b8e4ed4b908ac2f9b381f20a1feee35a936cf46
Colin Kennedy <colinvfx@gmail.com>
2023-06-23 19:36:46 -0700

    Added extended comment highlighting

--- lua/my_custom/plugins/manifest/appearance.lua
+++ lua/my_custom/plugins/manifest/appearance.lua
@@ -9,6 +9,13 @@ return {
             -- TODO: Get this color automatically
             vim.api.nvim_set_hl(0, "@namespace", {fg="#707880", ctermfg=243})
 
+            -- https://github.com/stsewd/tree-sitter-comment
+            --
+            -- Installed via ``:TSInstall comment``
+            --
+            vim.api.nvim_set_hl(0, "@text.danger", {link="SpellBad"})
+            vim.api.nvim_set_hl(0, "@text.note.rare", {link="SpellRare"})
+
             -- Show trailing whitespace as red text
             -- TODO: Get this color automatically
             vim.api.nvim_set_hl(0, "NonText", {bg="#5f0000", ctermbg=52})
