8b80488a8c421d4206ff29effc5c39f2b3ea69b1
Colin Kennedy <colinvfx@gmail.com>
2023-08-26 19:36:27 -0700

    Added toggleterm.nvim

--- lua/my_custom/plugins/manifest/workflow.lua
+++ lua/my_custom/plugins/manifest/workflow.lua
@@ -810,6 +810,7 @@ return {
                             A = "[S]witch [N]avigation",
                             S = "[S]witch [S]idebar",
                         },
+                        T = "Create a [T]erminal on the bottom of the current window.",
                         W = "Open [W]orkspace (NvimTree)",
                         Z = "[Z]oxide's interative pwd switcher",
                         c = {
@@ -861,5 +862,40 @@ return {
             })
         end,
         event = { "VeryLazy" },
+    },
+
+    -- A plugin that quickly makes and deletes Terminal buffers.
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = function()
+            vim.keymap.set(
+                "n",
+                "<space>T",
+                ":ToggleTerm direction=horizontal<CR>",
+                {
+                    desc="Create a [T]erminal on the bottom of the current window.",
+                    silent=true,
+                }
+            )
+
+            require("toggleterm").setup()
+
+            -- Important: This allows terminals to stay in terminal mode even
+            -- as you move in and out of them
+            --
+            function _G.set_terminal_keymaps()
+              local opts = {buffer = 0}
+              vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+              vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+              vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+              vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+              vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+              vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+              vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+            end
+            -- Note: If you only want these mappings for toggle term use term://*toggleterm#* instead
+            vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+        end
     }
 }
