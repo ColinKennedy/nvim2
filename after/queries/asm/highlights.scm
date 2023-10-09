5fe050840ba143a50c4a4f4a1c68a3369c6d94a7
Colin Kennedy <colinvfx@gmail.com>
2023-10-08 17:03:20 -0700

    Added WIP asm highlights

---
+++ after/queries/asm/highlights.scm
@@ -0,0 +1,25 @@
+;; extends
+;; https://www.reddit.com/r/neovim/comments/1255tw1/comment/je3t93u/?utm_source=share&utm_medium=web2x&context=3
+
+(comment) @comment
+(hexadecimal) @number
+(identifier) @variable
+(integer) @number
+(gcc_directive) @keyword.function
+(mnemonic) @keyword
+(placeholder) @constant
+(register) @variable.builtin
+(segment) @constant.builtin
+(string) @string
+(width) @attribute
+(label) @label
+
+":" @punctuation.delimiter
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+; TODO: Add these, later
+; "@" @punctuation.special
+; "<" @punctuation.bracket
+; ">" @punctuation.bracket
+; "(" @punctuation.bracket
+; ")" @punctuation.bracket
