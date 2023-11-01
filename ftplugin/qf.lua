f75c83421ed89b4d2b66f9d3690052f856281369
Colin Kennedy <colinvfx@gmail.com>
2023-10-31 21:45:10 -0700

    Added ftplugin/qf.lua

---
+++ ftplugin/qf.lua
@@ -0,0 +1,10 @@
+local protected_window = require("my_custom.utilities.protected_window")
+
+-- Make it so the quick-fix window will always display the quick-fix buffer
+protected_window.register_window_and_buffer(
+    vim.api.nvim_get_current_win(),
+    vim.fn.bufnr()
+)
+
+-- Move the quick-fix buffer to the top of the window
+vim.cmd[[wincmd K]]
