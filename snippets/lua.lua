aceb295f8245a6bab14bcb61a53da514c1978d39
Colin Kennedy <colinvfx@gmail.com>
2023-11-18 19:51:15 -0800

    Added lua snippets

--- snippets/lua.lua
+++ snippets/lua.lua
@@ -1,4 +1,6 @@
 local common_snippet = require("my_custom.snippets._common_snippet")
+local snippet_helper = require("my_custom.utilities.snippet_helper")
+local is_source_beginning = snippet_helper.is_source_beginning
 local luasnip = require("luasnip")
 local format = require("luasnip.extras.fmt").fmt
 local repeat_ = require("luasnip.extras").rep
@@ -75,6 +77,75 @@ luasnip.add_snippets(
                     repeat_(1),
                 }
             )
-        )
+        ),
+
+        snippet(
+            {
+                docstring="For statement",
+                trig="for",
+            },
+            format(
+                [[
+                    for {} in {}
+                    do
+                        {}
+                    end
+                ]],
+                {
+                    index(1, "item"),
+                    index(2, "items"),
+                    index(3, "-- Fill this in"),
+                }
+            ),
+            { show_condition = is_source_beginning("for") }
+        ),
+
+        snippet(
+            {
+                docstring="If statement",
+                trig="if",
+            },
+            format(
+                [[
+                    if {}
+                    then
+                        {}
+                    end
+                ]],
+                {
+                    index(1, "true"),
+                    index(2, "-- Fill this in"),
+                }
+            ),
+            { show_condition = is_source_beginning("if") }
+        ),
+
+        snippet(
+            {
+                docstring="return false",
+                trig="rf",
+            },
+            format(
+                [[
+                    return false
+                ]],
+                { }
+            ),
+            { show_condition = is_source_beginning("rf") }
+        ),
+
+        snippet(
+            {
+                docstring="return true",
+                trig="rt",
+            },
+            format(
+                [[
+                    return true
+                ]],
+                { }
+            ),
+            { show_condition = is_source_beginning("rt") }
+        ),
     }
 )
