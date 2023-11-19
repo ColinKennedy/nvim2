10a0a51a559d170d88a9925fab4173aab6773cff
Colin Kennedy <colinvfx@gmail.com>
2023-11-18 18:14:23 -0800

    Added :Messages command

--- lua/my_custom/start/command.lua
+++ lua/my_custom/start/command.lua
@@ -20,6 +20,21 @@ vim.api.nvim_create_user_command(
     }
 )
 
+vim.api.nvim_create_user_command(
+    "Messages",
+    function()
+        local bufnr = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_call(bufnr, function()
+            vim.cmd([[put= execute('messages')]])
+        end)
+        vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
+        vim.cmd.split()
+        local winnr = vim.api.nvim_get_current_win()
+        vim.api.nvim_win_set_buf(winnr, bufnr)
+    end,
+    {desc="Dump all (Neo)vim messages to a read-only buffer", nargs=0}
+)
+
 
 TAB_TERMINALS = {}
 
