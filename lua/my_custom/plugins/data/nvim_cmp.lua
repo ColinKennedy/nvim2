b8a773c840caeb4d3a99ed5a8d3cd89951452368
Colin Kennedy <colinvfx@gmail.com>
2024-02-06 22:00:26 -0800

    LuaSnip config change - exiting the current mode no longer exits snippet

--- lua/my_custom/plugins/data/nvim_cmp.lua
+++ lua/my_custom/plugins/data/nvim_cmp.lua
@@ -165,19 +165,3 @@ cmp.setup.filetype(
         )
     }
 )
-
--- Stop snippet expansion after leaving -- INSERT -- mode
---
--- Reference: https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1429989436
---
-vim.api.nvim_create_autocmd('ModeChanged', {
-    pattern = '*',
-    callback = function()
-        if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
-            and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
-            and not require('luasnip').session.jump_active
-        then
-            require('luasnip').unlink_current()
-        end
-    end
-})
