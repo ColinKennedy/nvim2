fedd165c96cb3f77cb102fe26b680bd7426be94b
Colin Kennedy <colinvfx@gmail.com>
2024-02-13 22:39:00 -0800

    Bug fix - <CR> in the quickfix works again

--- lua/my_custom/plugins/manifest/quick_fix.lua
+++ lua/my_custom/plugins/manifest/quick_fix.lua
@@ -12,40 +12,6 @@ return {
     -- Quickfix auto previews and other fun features
     {
         "kevinhwang91/nvim-bqf",
-        config = function()
-            vim.api.nvim_create_autocmd(
-                "FileType",
-                {
-                    callback = function()
-                        vim.keymap.set(
-                            "n",
-                            "<Enter>",
-                            function()
-                                local fixer = require("my_custom.utilities.quick_fix_selection_fix")
-
-                                -- Switch to the proper window for quick-fix to
-                                -- apply to. Then quickly switch back to the
-                                -- quick-fix buffer (because if we don't then
-                                -- `require("bqf.qfwin.handler").open(false)`
-                                -- will fail.
-                                --
-                                local current_window = vim.api.nvim_get_current_win()
-                                fixer.choose_last_window()
-                                vim.api.nvim_set_current_win(current_window)
-
-                                require("bqf.qfwin.handler").open(false)
-                            end,
-                            {
-                                nowait=true,
-                                buffer=true,
-                                desc="Select the current quick-fix entry.",
-                            }
-                        )
-                    end,
-                    pattern = "qf"
-                }
-            )
-        end,
         dependencies = {"junegunn/fzf", "nvim-treesitter/nvim-treesitter"},
         ft = "qf",
         version = "0.*",
