8e83decf4c7c324e6707edf2012e8c2a768b458d
Colin Kennedy <colinvfx@gmail.com>
2024-02-19 23:24:09 -0800

    Added more RST block title highlights

--- after/queries/rst/highlights.scm
+++ after/queries/rst/highlights.scm
@@ -1,4 +1,19 @@
 ;;extends
 
 (definition_list (list_item (term) @python_docstring_block_title
-  (#eq? @python_docstring_block_title "Args:")))
+  (#any-of? @python_docstring_block_title
+    "Args:"
+    "Example:"
+    "Examples:"
+    "Important:"
+    "Note:"
+    "Raises:"
+    "References:"
+    "Returns:"
+    "Returns:"
+    "See Also:"
+    "Todo:"
+    "Warning:"
+    "Warnings:"
+    "Yields:"
+  )))
