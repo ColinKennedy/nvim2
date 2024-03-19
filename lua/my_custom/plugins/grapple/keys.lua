3dbd3b6d469b9407e5d4b8e1189de4230bcf2477
Colin Kennedy <colinvfx@gmail.com>
2024-03-19 01:12:59 -0700

    Added grapple git_branch scope

--- lua/my_custom/plugins/grapple/keys.lua
+++ lua/my_custom/plugins/grapple/keys.lua
@@ -1,22 +1,22 @@
 return {
     {
         "<M-S-j>",
-        function() require("grapple").cycle_forward() end,
+        function() require("grapple").cycle_forward({scope="git_branch"}) end,
         desc = "Move to the next saved project path.",
     },
     {
         "<M-S-k>",
-        function() require("grapple").cycle_backward() end,
+        function() require("grapple").cycle_backward({scope="git_branch"}) end,
         desc = "Move to the previous saved project path.",
     },
     {
         "<M-S-l>",
-        function() require("grapple").open_tags("git") end,
+        function() require("grapple").open_tags({scope="git_branch"}) end,
         desc = "Show all saved project paths.",
     },
     {
         "<M-S-h>",
-        function() require("grapple").toggle({ scope="git" }) end,
+        function() require("grapple").toggle({scope="git_branch"}) end,
         desc = "Add / Remove the current file as a project path.",
     },
     {
