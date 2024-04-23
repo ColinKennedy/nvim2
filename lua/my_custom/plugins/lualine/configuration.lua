4f313cd5be1e1a08c10e2bee64b967f106f99196
Colin Kennedy <colinvfx@gmail.com>
2024-04-22 18:00:21 -0700

    Added consistent indentation

--- lua/my_custom/plugins/lualine/configuration.lua
+++ lua/my_custom/plugins/lualine/configuration.lua
@@ -1,39 +1,38 @@
 local utils = require("lualine.utils.utils")
 
 require("lualine").setup {
-  options = {
-    icons_enabled = true,
-    theme = "onedark",
-    section_separators = { left = "", right = ""},
-    component_separators = { left = '', right = ''},
-  },
-  sections = {
-    lualine_b = {"branch"},
-    lualine_c = { "grapple" },
-    lualine_x = {},
-    lualine_y = {
-        {
-            "diagnostics",
-            diagnostics_color = {
-                -- These values go good with hybrid2. Maybe add lualine to the theme?
-
-                -- warn = 'DiagnosticWarning',
-                warn = {
-                  fg = utils.extract_color_from_hllist(
-                    { "fg", "sp" }, { "DiagnosticWarning" }, "#ffcc00"
-                  ),
+    options = {
+        icons_enabled = true,
+        theme = "onedark",
+        section_separators = { left = "", right = ""},
+        component_separators = { left = '', right = ''},
+    },
+    sections = {
+        lualine_b = {"branch"},
+        lualine_c = { "grapple" },
+        lualine_x = {},
+        lualine_y = {
+            {
+                "diagnostics",
+                diagnostics_color = {
+                    -- TODO: These values go good with hybrid2. Maybe add lualine to the theme?
+                    -- warn = 'DiagnosticWarning',
+                    warn = {
+                        fg = utils.extract_color_from_hllist(
+                          { "fg", "sp" }, { "DiagnosticWarning" }, "#ffcc00"
+                        ),
+                    },
+                },
+                symbols = {
+                    -- Reference: www.nerdfonts.com/cheat-sheet
+                    error = " ",
+                    warn = " ",
+                    info = " ",
+                    hint = " ",
                 },
             },
-            symbols = {
-                -- Reference: www.nerdfonts.com/cheat-sheet
-                error = " ",
-                warn = " ",
-                info = " ",
-                hint = " ",
-            },
+            "progress",
         },
-        "progress",
+        lualine_z = {"location"}
     },
-    lualine_z = {"location"}
-  },
 }
