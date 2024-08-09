32b8bdda434986f951108db72c0b5281ed0e5d0b
Colin Kennedy <colinvfx@gmail.com>
2024-08-09 13:38:21 -0700

    Updated lsp_signature.nvim

--- lua/my_custom/plugins/which_key/configuration.lua
+++ lua/my_custom/plugins/which_key/configuration.lua
@@ -7,12 +7,22 @@ which_key.add(
         {"<leader>d", group = "+[d]ebug prefix"},
         {"<leader>dg", group = "+[d]ebug lo[g] prefix"},
         {"<leader>g", group = "+[g]it prefix"},
+        {"<leader>ga", group = "+[g]it [a]dd prefix"},
+        {"<leader>gc", group = "+[g]it [c]heckout prefix"},
+        {"<leader>gd", group = "+[g]it [d]iff prefix"},
+        {"<leader>gp", group = "+[g]it [p]ush/[p]ull prefix"},
+        {"<leader>gr", group = "+[g]it [r]eset prefix"},
+        {"<leader>gs", group = "+[g]it [s]tash prefix"},
         {"<leader>i", group = "+[i]nsert prefix"},
         {"<leader>r", group = "+[r]un prefix"},
-        {"<leader>s", group = "+[m]isc prefix"},
+        {"<leader>ri", group = "+[r]efactor [i]nline prefix"},
+        {"<leader>rt", group = "+[t]oggle [t]est prefix"},
+        {"<leader>s", group = "+misc prefix"},
         {"<leader>t", group = "+[t]oggle prefix"},
+        {"<leader>tn", group = "+[t]oggle [n]eotest prefix"},
 
         {"<Space>", group = "Space Sitching Mappings"},
+        {"<Space>G", group = "[G]it related"},
         {"<Space>S", group = "[S]witcher aerial.nvim windows"},
     }
 )
