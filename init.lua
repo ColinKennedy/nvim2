97441a139a5f6902857ebb39df39c83d79b89a9c
Colin Kennedy <colinvfx@gmail.com>
2024-12-14 00:12:16 -0800

    Added temp plugins

--- init.lua
+++ init.lua
@@ -52,6 +52,224 @@ vim.g.vim_home = filer.get_current_directory()
 
 require("my_custom.start.fix_terminal_padding")
 
+table.insert(
+    plugins,
+    {
+        "ColinKennedy/nvim-best-practices-plugin-template",
+        dependencies = { "ColinKennedy/mega.cmdparse", "ColinKennedy/mega.logging" },
+        -- TODO: (you) - Make sure your first release matches v1.0.0 so it auto-releases! ]]
+        version = "v1.*",
+    }
+)
+
+table.insert(
+    plugins,
+    {
+        "ColinKennedy/mcp.nvim",
+        version = "v1.*",
+    }
+)
+
+-- table.insert(
+--     plugins,
+--     {
+--         "ColinKennedy/cmdparse.nvim",
+--         version = "v1.*",
+--     }
+-- )
+
+table.insert(
+    plugins,
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          -- { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    }
+)
+
+table.insert(
+    plugins,
+    {
+        "echasnovski/mini.doc"
+    }
+)
+
+table.insert(
+    plugins,
+    {
+        "ColinKennedy/mega.vimdoc",
+        dependencies = { "echasnovski/mini.doc" },
+    }
+)
+
+table.insert(
+    plugins,
+    {
+        "ColinKennedy/mega.speedreader",
+    }
+)
+
+-- table.insert(
+--     plugins,
+--     {
+--         'echasnovski/mini.bracketed',
+--         config = function()
+--             require("mini.bracketed").setup()
+--
+--             vim.api.nvim_set_keymap('n', '<C-A-j>', '[t', { expr = true, silent = true })
+--             vim.api.nvim_set_keymap('n', '<C-A-k>', ']t', { expr = true, silent = true })
+--             vim.api.nvim_set_keymap('n', '<C-A-h>', '[T', { expr = true, silent = true })
+--             vim.api.nvim_set_keymap('n', '<C-A-l>', ']T', { expr = true, silent = true })
+--         end,
+--     }
+-- )
+
+-- table.insert(
+--     plugins,
+--     {
+--         "JellyApple102/easyread.nvim",
+--         -- config = true,
+--         config = function()
+--             require('easyread').setup{
+--                 hlgroupOptions = { bold = true },
+--                 fileTypes = {'md', 'markdown'}
+--             }
+--         end,
+--     }
+-- )
+
+
+-- table.insert(
+--     plugins,
+--     {
+--       "karb94/neoscroll.nvim",
+--       config = function ()
+--         require('neoscroll').setup({
+--             mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
+--               '<C-u>', '<C-d>',
+--               '<C-b>', '<C-f>',
+--               '<C-y>', '<C-e>',
+--               'zt', 'zz', 'zb',
+--             },
+--         })
+--       end
+--     }
+-- )
+
+
+-- table.insert(
+--     plugins,
+--     {
+--         "ColinKennedy/vim-ninja-feet",
+--         branch = "add_extra_visual_operators",
+--     }
+-- )
+
+-- table.insert(
+--     plugins,
+--     {
+--       'deparr/tairiki.nvim',
+--       lazy = false,
+--       priority = 1000, -- only necessary if you use tairiki as default theme
+--       config = function()
+--         require('tairiki').setup {
+--           -- optional configuration here
+--         }
+--         require('tairiki').load() -- only necessary to use as default theme, has same behavior as ':colorscheme tairiki'
+--       end,
+--     }
+-- )
+--
+--
+-- table.insert(
+--     plugins,
+--     {
+--         "w0ng/vim-hybrid",
+--     }
+-- )
+
+-- table.insert(
+--     plugins,
+--     {
+--         "oguzbilgic/vim-gdiff",
+--     }
+-- )
+
+-- table.insert(
+--     plugins,
+--     {
+--       'pwntester/octo.nvim',
+--       dependencies = {
+--         'nvim-lua/plenary.nvim',
+--         'nvim-telescope/telescope.nvim',
+--         -- OR 'ibhagwan/fzf-lua',
+--         'nvim-tree/nvim-web-devicons',
+--       },
+--       config = function ()
+--         require"octo".setup()
+--       end
+--     }
+-- )
+
+table.insert(
+    plugins,
+    {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            vim.cmd[[set termguicolors]]
+            require'colorizer'.setup()
+        end
+    }
+)
+
+
+-- table.insert(
+--     plugins,
+--     {
+--       "folke/snacks.nvim",
+--       priority = 1000,
+--       lazy = false,
+--       opts = function()
+--         -- Toggle the profiler
+--         Snacks.toggle.profiler():map("<leader>pp")
+--         -- Toggle the profiler highlights
+--         Snacks.toggle.profiler_highlights():map("<leader>ph")
+--       end
+--       --   -- your configuration comes here
+--       --   -- or leave it empty to use the default settings
+--       --   -- refer to the configuration section below
+--       --   bigfile = { enabled = true },
+--       --   notifier = { enabled = true },
+--       --   quickfile = { enabled = true },
+--       --   statuscolumn = { enabled = true },
+--       --   words = { enabled = true },
+--       -- },
+--     }
+-- )
+
+table.insert(
+    plugins,
+    {
+        "shortcuts/no-neck-pain.nvim",
+        version = "*",
+    }
+)
+
+table.insert(
+    plugins,
+    -- lazy.nvim
+    {
+        "chrisgrieser/nvim-tinygit",
+        dependencies = "stevearc/dressing.nvim",
+    }
+)
+
 extend(plugins, require("my_custom.plugins.manifest.appearance"))
 extend(plugins, require("my_custom.plugins.manifest.debugging"))
 extend(plugins, require("my_custom.plugins.manifest.lsp"))
@@ -104,3 +322,25 @@ require("my_custom.start.lsp_diagnostics")
 require("my_custom.start.saver").initialize()
 require("my_custom.utilities.quick_fix_selection_fix").initialize()
 vim.cmd("source " .. vim.g.vim_home .. "/plugin/miscellaneous_commands.vim")
+
+-- NOTE: Requires https://github.com/shortcuts/no-neck-pain.nvim
+pcall(function() vim.cmd[[NoNeckPain]] end)
+
+-- vim.schedule(
+--     function()
+--         local profile = require("profile")
+--         print('running')
+--
+--         profile.start("*")
+--
+--         local options = {}
+--
+--         for index=1,1 do
+--             vim.tbl_extend("force", options, { {foo={another=8}} })
+--             -- vim.tbl_deep_extend("force", options, { {foo={another=8}} })
+--         end
+--
+--         profile.export("out.json")
+--     end
+-- )
+
