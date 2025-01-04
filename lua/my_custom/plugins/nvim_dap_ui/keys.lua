8e5f58e33c9a999f08aeeaa3a680382d19e750c1
Colin Kennedy <colinvfx@gmail.com>
2025-01-04 08:28:20 -0800

    Fixed nvim-dap-ui initialization

--- lua/my_custom/plugins/nvim_dap_ui/keys.lua
+++ lua/my_custom/plugins/nvim_dap_ui/keys.lua
@@ -13,7 +13,14 @@ return {
         function()
             require("dapui").open()  -- Requires nvim-dap-ui
 
-            vim.cmd[[DapContinue]]  -- Important: This will lazy-load nvim-dap
+            -- IMPORTANT: This does two things at once
+            --
+            -- 1. Calling DapContinue will lazy-load nvim-dap
+            -- 2. We vim.schedule because DapContinue may spawn a GUI but we
+            --    need that GUI to spawn after dapui.open() is 100% finished.
+            --    vim.schedule will make sure of this.
+            --
+            vim.schedule(function() vim.cmd[[DapContinue]] end)
         end,
         desc="[d]o [d]ebugger. Start a debugging session.",
     },
