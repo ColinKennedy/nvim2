b1adbec11d6ee2e2bdaba113944a24f1794cfbee
Colin Kennedy <colinvfx@gmail.com>
2025-01-27 15:39:13 -0800

    Fixed issue where sometimes nomodifiable is set on the wrong buffer

--- lua/my_custom/start/command.lua
+++ lua/my_custom/start/command.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_user_command("Messages", function()
     vim.api.nvim_buf_call(bufnr, function()
         vim.cmd([[put= execute('messages')]])
     end)
-    vim.bo.modifiable = false
+    vim.bo[bufnr].modifiable = false
     vim.cmd.split()
     local winnr = vim.api.nvim_get_current_win()
     vim.api.nvim_win_set_buf(winnr, bufnr)
