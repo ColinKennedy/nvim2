c2bb62aeabea9b82a7a1ecc8badcfa359876678d
Colin Kennedy <colinvfx@gmail.com>
2026-01-18 22:38:17 -0800

    Added .github/scripts/load_plugins.lua

---
+++ .github/scripts/load_plugins.lua
@@ -0,0 +1,13 @@
+--- Force-load all Lazy plugins at once.
+---
+--- We do this to confirm:
+---
+--- 1. That all of the plugins are present
+--- 2. Validate that the plugins load as expected
+--- 3. Fail CI if something went wrong
+---
+
+print("ALL SOURCED PATHS")
+-- TODO: Remove this later
+vim.cmd.scriptnames()
+vim.cmd("Lazy load all")
